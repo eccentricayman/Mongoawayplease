e2eee5d578065f46dd9a578af417f75b375ff6d7
eccentricayman <aahmed3@stuy.edu>
2017-02-06 12:33:04 -0500

    lul

--- db_readah.py
+++ db_readah.py
@@ -2,3 +2,11 @@ from pymongo import MongoClient
 
 #server = MongoClient('149.89.150.100')
 server = MongoClient('127.0.0.1')
+
+db = server.RefinedStorage
+col = db.students
+
+cur = col.find()
+
+for i in cur:
+    
